2a180a0b4710dde8686362ad66da02890e34f71f
Michael Meister <m.meister@cje.de>
2018-02-14 10:55:12 +0100

    tut done

--- ASPNetCoreReact/wwwroot/src/app.js
+++ ASPNetCoreReact/wwwroot/src/app.js
@@ -1,3 +1,28 @@
-﻿require('./lib');
+﻿
+require('./lib');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/site.css';
+//import ES6Lib from './es6codelib';
 
-document.getElementById("fillThis").innerHTML = getText();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './reactcomponent';
+import FetchData from './fetchdata';
+
+ReactDOM.render(
+    <Counter />,
+    document.getElementById('basicreactcomponent')
+);
+
+ReactDOM.render(
+	<FetchData />,
+	document.getElementById('reactcomponentwithapidata')
+);
+
+//document.getElementById("fillThis").innerHTML = getText();
+
+//$('#fillThisWithjQuery').html('filled by jQuery.');
+//let myES6Object = new ES6Lib();
+//$('#fillThisWithES6Lib').html(myES6Object.getData());
+
+module.hot.accept();
